1a903e58db3d79ecdf883979ac623799f6330398
Jeffrey Heller <heller.jeffrey@gmail.com>
2018-11-09 20:18:00 -0500

    render chess board in template with starting position

--- controllers/openings.js
+++ controllers/openings.js
@@ -7,8 +7,8 @@ const Chess = require('chess.js').Chess
 router.get('/:id', (req, res) => {
   Opening.findOne({ _id: req.params.id })
     .then(opening => {
-      const chess = new Chess()
-      res.render('openings/show', { opening: opening, chess: chess })
+      let board = generateBoard()
+      res.render('openings/show', { opening: opening, board: board })
     })
     .catch(err => {
       res.render('error', { error: err })
@@ -28,17 +28,61 @@ router.get('/', (req, res) => {
 
 module.exports = router
 
-let chess = new Chess()
-var str = chess.ascii()
-var ascii = str.split('').map(itm => {
-  return itm.charCodeAt(0)
-})
-var str2 = ascii
-  .map((itm, i) => {
-    return (
-      '#' + i + '=0x' + itm.toString(16) + ' (' + String.fromCharCode(itm) + ')'
-    )
-  })
-  .join('\n')
+const generateBoard = () => {
+  let chess = new Chess()
+  var str = chess.ascii()
 
-console.log('original string=' + str + '\nascii codes:\n' + str2)
+  var ascii = str.split('').map(itm => {
+    return itm.charCodeAt(0)
+  })
+
+  let board = []
+
+  ascii.forEach(code => {
+    switch (String.fromCharCode(code)) {
+      case '.':
+        board.push(String.fromCharCode(code))
+        break
+      case 'p':
+        board.push(String.fromCharCode(code))
+        break
+      case 'P':
+        board.push(String.fromCharCode(code))
+        break
+      case 'r':
+        board.push(String.fromCharCode(code))
+        break
+      case 'R':
+        board.push(String.fromCharCode(code))
+        break
+      case 'n':
+        board.push(String.fromCharCode(code))
+        break
+      case 'N':
+        board.push(String.fromCharCode(code))
+        break
+      case 'b':
+        board.push(String.fromCharCode(code))
+        break
+      case 'B':
+        board.push(String.fromCharCode(code))
+        break
+      case 'q':
+        board.push(String.fromCharCode(code))
+        break
+      case 'Q':
+        board.push(String.fromCharCode(code))
+        break
+      case 'k':
+        board.push(String.fromCharCode(code))
+        break
+      case 'K':
+        board.push(String.fromCharCode(code))
+        break
+      default:
+        break
+    }
+  })
+
+  return board
+}
