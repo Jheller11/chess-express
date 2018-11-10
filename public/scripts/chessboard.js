79b150f3ad6c7802be6f531c6db9eec4e7579f29
Jeffrey Heller <heller.jeffrey@gmail.com>
2018-11-10 18:26:03 -0500

    add images

--- public/scripts/chessboard.js
+++ public/scripts/chessboard.js
@@ -7,13 +7,18 @@ class ChessBoard {
   }
 
   render() {
-    for (let i = 0; i < 64; i++) {
-      let square = document.createElement('div')
-      if (i % 2 === 1) {
-        this.element.appendChild(square)
-      } else {
-        square.setAttribute('style', 'background-color: red')
-        this.element.appendChild(square)
+    for (let i = 0; i < 8; i++) {
+      for (let j = 0; j < 8; j++) {
+        let square = document.createElement('div')
+        if ((i + j) % 2 === 0) {
+          this.element.appendChild(square)
+        } else {
+          square.setAttribute(
+            'style',
+            'background-color: rgba(0, 100, 100, .5)'
+          )
+          this.element.appendChild(square)
+        }
       }
     }
   }
