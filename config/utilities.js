2fd9b795993f216133e0ccef05c8e5ca19857138
Jeffrey Heller <heller.jeffrey@gmail.com>
2018-11-14 17:13:14 -0500

    debug error handling

--- config/utilities.js
+++ config/utilities.js
@@ -4,20 +4,16 @@ const utilities = {
   // verify current user is admin
   isAdminUser: (req, res, next) => {
     if (req.isAuthenticated() && req.user.admin) return next()
-    else {
-      res.redirect('/error', {
-        error: 'Please log in to an admin account to access this page.'
-      })
-    }
+    res.render('error', {
+      error: 'Please log in to an admin account to access this page.'
+    })
   },
   // verify current user is logged in
   isLoggedIn: (req, res, next) => {
     if (req.isAuthenticated()) return next()
-    else {
-      res.redirect('/error', {
-        error: 'You must be logged in to access this page.'
-      })
-    }
+    res.render('error', {
+      error: 'You must be logged in to access this page.'
+    })
   },
   // generate board array
   generateBoard: chess => {
