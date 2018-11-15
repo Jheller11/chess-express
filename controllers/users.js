d60ec96c4f02f66daef21a0d081acb4b0cde23c5
Jeffrey Heller <heller.jeffrey@gmail.com>
2018-11-15 18:43:03 -0500

    config form and route for adding game to user

--- controllers/users.js
+++ controllers/users.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const User = require('../models/User')
 const passport = require('passport')
 require('../config/passport')(passport)
+const isLoggedIn = require('../config/utilities').isLoggedIn
 
 // user login route
 router.post(
@@ -23,6 +24,24 @@ router.post(
     failureFlash: true
   })
 )
+
+// add a new game to existing user
+router.post('/:id', (req, res) => {
+  User.findOne({ _id: req.params.id })
+    .then(user => {
+      user.games.push(req.body.game)
+      user.save()
+    })
+    .then(() => {
+      res.redirect('/users/profile')
+    })
+    .catch(err => {
+      res.redirect('/users/profile', {
+        message:
+          'There was an error saving the game. Please ensure the PNG is complete.'
+      })
+    })
+})
 
 // delete user route
 router.delete('/:id', (req, res) => {
@@ -52,7 +71,7 @@ router.get('/signup', (req, res) => {
 })
 
 // user profile page
-router.get('/profile', (req, res) => {
+router.get('/profile', isLoggedIn, (req, res) => {
   res.render('users/profile')
 })
 
